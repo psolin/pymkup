fa84acbc20b1f65a8b3d88942de43a7ff112a445
supersonic1999 <jason0gray@googlemail.com>
2021-04-29 23:05:49 +0100

    adjusted test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,11 +1,14 @@
 from pymkup import pymkup
+import pytest
 
-def spaces_returned_dict():
-	x = pymkup("\\tests\\markup-deep-spaces.pdf")
+pytest.filename = "\\tests\\markup-deep-spaces.pdf"
+
+def test_spaces_returned_dict():
+	x = pymkup(pytest.filename)
 	spaces = x.spaces()
 	assert 'spaces' in spaces
 
-def markup_returned_dict():
-	x = pymkup("\\tests\\markup-deep-spaces.pdf")
+def test_markup_returned_dict():
+	x = pymkup(pytest.filename)
 	markup = x.markups()
 	assert 'markups' in markup
