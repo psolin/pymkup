20903f9f3a00fe75a1de541a61d2394b5f49a498
Paul <paulsolin@gmail.com>
2021-04-28 18:00:52 -0600

    PyPI 0.4 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pymkup",
-    version="0.3",
+    version="0.4",
     description="Accessing Bluebeam Revu PDF Data",
     long_description=README,
     long_description_content_type="text/markdown",
