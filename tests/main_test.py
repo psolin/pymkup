470a2c0ef51515ce553bcff2c8a7b6d1bb1da8d2
Paul <paulsolin@gmail.com>
2021-05-02 14:35:07 -0600

    Tests fixed

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,17 +1,17 @@
 import pytest
 
-from pymkup import pymkup
+from pymkup import Pymkup
 
 pytest.filename = "/tests/markup-deep-spaces.pdf"
 
 
 def test_spaces_returned_dict():
-    x = pymkup(pytest.filename)
+    x = Pymkup(pytest.filename)
     spaces = x.spaces()
     assert 'spaces' in spaces
 
 
 def test_markup_returned_dict():
-    x = pymkup(pytest.filename)
+    x = Pymkup(pytest.filename)
     markup = x.markups()
     assert 'markups' in markup
